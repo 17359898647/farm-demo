023905f4b2bd1856275dfca108858ec0b05661df
YangyangHuang <yangyang@weimill.com>
2025-04-16 21:05:40 -0600

    remove

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,3 @@
     <i class="icon-material-symbols:10k" ></i>
   </div>
 </template>
-
-<style scoped >
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #9F1A8Faa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
